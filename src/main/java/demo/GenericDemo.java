6a6d62f93f13c53178b74f09bbfbcf6477958b46
xuan <1>
2021-01-12 19:37:11 +0800

    1、ClassUtil 2、单例模式demo

--- src/main/java/demo/GenericDemo.java
+++ src/main/java/demo/GenericDemo.java
@@ -1,12 +1,31 @@
 package demo;
 
 
+import org.omg.CORBA.INTERNAL;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class GenericDemo {
     public static void main(String[] args) {
-        System.out.println("态");
-        System.out.println((char)36896);
+//        Integer a = 1;
+//        Integer b = 1;
+//        Integer c = 128;
+//        Integer d = 12833;
+//        Integer d2 = 12933;
+//        System.out.println(a==b);
+//        System.out.println(c.equals(d));
+//        Integer e = a + b;
+//        Integer f = c +d;
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(c>d2);
+//        List<String> list = new ArrayList<>();
+//        Stream<String> stringStream = list.stream().filter(String::isEmpty);
+        Integer aa = null;
+        Integer bb = 2;
+        System.out.println(aa+bb);
     }
 
 }
